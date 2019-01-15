ef433e956bdd235cbd762418e8a37cb6d6fb66d3
csPython <cspython@AD25TF04NGF1J-42.local>
2019-01-15 11:21:18 -0500

    2 working questions

--- quiz1NC.py
+++ quiz1NC.py
@@ -1,37 +1,27 @@
 #QuizNC.py
+from quizNCvardef import *
 
-def run_quest(quest, ansU, check, ansR, grade):
-    while quest == False: #while the question hasn't been answered
-    try:
-        ansU = int(input(quest)) #ask the question
-        print("")
-        if ansR == int(3): #if the user gets the right answer
-            quest = True #change the state of the question. make it true
-            grade += 1 #add to the score
-            print("Correct.")
-            print("Move on to the next question!")
-        
+def run_quest(quest, ansU, check, ansR):
+    while check == False: #while the question hasn't been answered
+        try:
+            ansU = int(input(quest)) #ask the question
+            print("")
+            if ansU == ansR: #if the user gets the right answer
+                print("Move on to the next question!")
+                print("")
+                check = True #change the state of the question. make it true
 
-        elif 0 < ansU < 5: #if the answer is one of the choices but wrong
-            print("Sorry your answer is not correct. The answer was 3.\n")
-            print("Please move on to the next question.")
-            break #break the loop.
+            elif 0 < ansU < 5: #if the answer is one of the choices but wrong
+                print("Please move on to the next question.")
+                print("")
+                break #break the loop.
+            else:
+                print("Please put the number of one of the answer choice\n")
 
-    except ValueError: #if they inputted anything else
-        print("")
-        print("Please put the number of the answer choice\n") #tell directions
-  
-    
-q1 = bool(False)
-score = int(0) # the score
-q1ans = int(0) # the answer to user inputs
-q1ask = str("""1. What is the most well-known coding language?\n
-            1. Elixir
-            2. Python
-            3. JavaScript
-            4. Swift\n""")
-print("")
-print("please only type the number of the answer choice you have chosen.")
-print("")      
+        except ValueError: #if they inputted anything else
+            print("")
+            print("That is not an acceptable answer!")
+            print("Please put the number of the answer choice\n") #tell directions
 
-        
+run_quest(q1, ansU, check, q1CorAns)
+run_quest(q2, ansU, check, q2CorAns)
---
+++ quizNCvardef.py
@@ -0,0 +1,22 @@
+#quiz defining variables
+
+
+ansU = str("")
+check = bool(False)
+
+
+q1 = str("""1. Which of these has a talking Sponge?\n
+            1. Fairly Odd Parents
+            2. Shrek
+            3. Spongebob Squarepants
+            4. Total Drama Island\n""")
+q1CorAns = int(3)
+q1Ans = int()
+
+q2 = str("""2. Which of these is about three girls who are superheroes?\n
+            1. Chowder
+            2. Powerpuff Girls
+            3. Pucca
+            4. Tuff Puppy\n""")
+q2CorAns = int(2)
+q2Ans = int()
