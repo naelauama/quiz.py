fccd35b55b272a5de888a44f81705b30ac24cdd2
csPython <cspython@AD25TF04NGF1J-42.local>
2019-01-18 09:28:05 -0500

    all questions, score

--- quizNCvardef.py
+++ quizNCvardef.py
@@ -1,6 +1,9 @@
 #quiz defining variables
 
+global SCORE
+SCORE = int()
 
+grade = int()
 ansU = str("")
 check = bool(False)
 
@@ -20,3 +23,72 @@ q2 = str("""2. Which of these is about three girls who are superheroes?\n
             4. Tuff Puppy\n""")
 q2CorAns = int(2)
 q2Ans = int()
+
+q3 = str("""3. Which of these has a main character that lives in a whale?\n
+            1. Total Drama Island
+            2. Chowder
+            3. My Life as a Teenage Robot
+            4. The Misadventures of Flapjack\n""")
+q3CorAns = int(4)
+q3Ans = int()
+
+q4 = str("""4. Which show has 3 judges?\n
+            1. Hell's Kitchen
+            2. Chopped
+            3. Guy's Grocery Games
+            4. 30 Minute Meals\n""")
+
+q4CorAns = int(2)
+q4Ans = int()
+
+q5 = str("""5. Which show is about saving a failed business?\n
+            1. Bar Rescue
+            2. Cake Boss
+            3. The Great British Bake Off
+            4. Lucky Dog\n""")
+q5CorAns = int(1)
+q5Ans = int()
+
+q6 = str("""6. What show has an autistic doctor as the main character?\n
+            1. Black Mirror
+            2. Grey's Anatomy
+            3. The Good Doctor
+            4. General Hospital\n""")
+q6CorAns = int(3)
+q6Ans = int()
+
+q7 = str("""7. What show was the first on disney to have a lesbian couple?\n
+            1. Girl Meets World
+            2. Good Luck Charlie
+            3. Shake it Up
+            4. Jessie\n""")
+q7CorAns = int(2)
+q7Ans = int()
+
+q8 = str("""8. Which shows is about a boy band from Minnesota?\n
+            1. Big Time Rush
+            2. Victorious
+            3. Big Bang Theory
+            4. Drake & Josh\n""")
+q8CorAns = int(1)
+q8Ans = int()
+
+q9 = str("""9. What show is about a male who stalks a girl\n   he's in love with?\n
+            1. Pretty Little Liars
+            2. Stalker
+            3. The Guardian
+            4. You\n""")
+q9CorAns = int(4)
+q9Ans = int()
+
+q10 = str("""10. Which is a netflix original show based around puberty?\n
+              1. Insatiable
+              2. Big Mouth
+              3. To All the Boys I've Loved Before
+              4. Bright\n""")
+q10CorAns = int(2)
+q10Ans = int()
+
+
+          
+     
